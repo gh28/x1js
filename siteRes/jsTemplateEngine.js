5737dcd5bd788a4a1f6a8774c7f1cbf207a02e11
tanghao <tanghao@jidemail.com>
2015-02-27 13:55:12 +0800

    refine javasript template engine

--- siteRes/jsTemplateEngine.js
+++ siteRes/jsTemplateEngine.js
@@ -1,39 +1,65 @@
 (function() {
-  var cache = {};
+	// should keep cache transparent
+	var cache = {};
 
-  /**
-   * jsTemplate is a string mainly html with javascript code in <% %> and <%= %> block
-   */
-  function generateRenderer(jsTemplate) {
-    jsTemplate = jsTemplate
-      .replace(/[\r\t\n]/g, " ")
-      .split("<%").join("\t")
-      .replace(/((^|%>)[^\t]*)'/g, "$1\r")
-      .replace(/\t=(.*?)%>/g, "',$1,'")
-      .split("\t").join("');")
-      .split("%>").join("p.push('")
-      .split("\r").join("\\'");
-    return new Function("data", "  \
-        var p = []; \
-        var print = function() {  \
-          p.push.apply(p, arguments); \
-        };  \
-        with(data) { \
-          p.push('" + jsTemplate + "'); \
-        } \
-        return p.join('');");
-  };
+	/**
+	 * compile jsTemplate to a render function
+	 * jsTemplate is a string mainly html with javascript code in <% %> and <%= %> block
+	 */
+	function compile(jsTemplate) {
+		jsTemplate = jsTemplate
+			.replace(/[\r\t\n]/g, " ")
+			.split("<%").join("\t")
+			.replace(/((^|%>)[^\t]*)'/g, "$1\r")
+			.replace(/\t=(.*?)%>/g, "',$1,'")
+			.split("\t").join("');")
+			.split("%>").join("p.push('")
+			.split("\r").join("\\'");
+		var fn = new Function("data", "  \
+			var p = []; \
+			var print = function() {  \
+				p.push.apply(p, arguments); \
+			};  \
+			with(data) { \
+				p.push('" + jsTemplate + "'); \
+			} \
+			return p.join('');");
+		return {
+			render: fn
+		};
+	}
 
-  this.instantiate = function(s, data) {
-    if (/\W/.test(s)) {
-      // it contains [^0-9A-Z_a-z], not to cache
-      return generateRenderer(s)(data);
-    }
+	function compileByDomId(domId) {
+		if (/[^-0-9A-Z_a-z]/.test(domId)) {
+			throw new Error("invalid tag id");
+		}
 
-    // it is an id for a tag
-    var id = s;
-    s = document.getElementById(id).innerHTML;
-    cache[id] = cache[id] || generateRenderer(s);
-    return cache[id](data);
-  };
+		var domElement = document.getElementById(domId);
+		if (!domElement) {
+			throw new Error("cannot find dom id: " + id);
+		}
+
+		return compile(domElement.innerHTML);
+	}
+
+	function instantiate(jsTemplate, data) {
+		return compile(jsTemplate).render(data);
+	}
+
+	function instantiateById(domId, data) {
+		if (/[^-0-9A-Z_a-z]/.test(domId)) {
+			throw new Error("invalid dom id");
+		}
+
+		// lookup cache first
+		if (typeof cache[domId] === "undefined") {
+			cache[domId] = compileByDomId(domId);
+		}
+		return cache[domId].render(data);
+	}
+
+	this.jsTemplateEngine = {
+		instantiate: instantiate,
+		instantiateById: instantiateById
+	};
 })();
