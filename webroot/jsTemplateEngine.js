19a27ea4a2bb4673813ee54c8a722eff55390c1d
tanghao <th4416@hotmail.com>
2015-10-10 09:56:46 +0800

    remark a bug: jsTemplateEngine.instantiateById doesn't work

--- webroot/jsTemplateEngine.js
+++ webroot/jsTemplateEngine.js
@@ -1,65 +1,66 @@
 (function() {
-	// should keep cache transparent
-	var cache = {};
+    // should keep cache transparent
+    var cache = {};
 
-	/**
-	 * compile jsTemplate to a render function
-	 * jsTemplate is a string mainly html with javascript code in <% %> and <%= %> block
-	 */
-	function compile(jsTemplate) {
-		jsTemplate = jsTemplate
-			.replace(/[\r\t\n]/g, " ")
-			.split("<%").join("\t")
-			.replace(/((^|%>)[^\t]*)'/g, "$1\r")
-			.replace(/\t=(.*?)%>/g, "',$1,'")
-			.split("\t").join("');")
-			.split("%>").join("p.push('")
-			.split("\r").join("\\'");
-		var fn = new Function("data", "  \
-			var p = []; \
-			var print = function() {  \
-				p.push.apply(p, arguments); \
-			};  \
-			with(data) { \
-				p.push('" + jsTemplate + "'); \
-			} \
-			return p.join('');");
-		return {
-			render: fn
-		};
-	}
+    /**
+     * compile jsTemplate to a render function
+     * jsTemplate is a string mainly html with javascript code in <% %> and <%= %> block
+     */
+    function compile(jsTemplate) {
+        jsTemplate = jsTemplate
+            .replace(/[\r\t\n]/g, " ")
+            .split("<%").join("\t")
+            .replace(/((^|%>)[^\t]*)'/g, "$1\r")
+            .replace(/\t=(.*?)%>/g, "',$1,'")
+            .split("\t").join("');")
+            .split("%>").join("p.push('")
+            .split("\r").join("\\'");
+        var fn = new Function("data", "  \
+            var p = []; \
+            var print = function() {  \
+                p.push.apply(p, arguments); \
+            };  \
+            with(data) { \
+                p.push('" + jsTemplate + "'); \
+            } \
+            return p.join('');");
+        return {
+            render: fn
+        };
+    }
 
-	function compileByDomId(domId) {
-		if (/[^-0-9A-Z_a-z]/.test(domId)) {
-			throw new Error("invalid tag id");
-		}
+    function compileByDomId(domId) {
+        if (/[^-0-9A-Z_a-z]/.test(domId)) {
+            throw new Error("invalid tag id");
+        }
 
-		var domElement = document.getElementById(domId);
-		if (!domElement) {
-			throw new Error("cannot find dom id: " + domId);
-		}
+        var domElement = document.getElementById(domId);
+        if (!domElement) {
+            throw new Error("cannot find dom id: " + domId);
+        }
 
-		return compile(domElement.innerHTML);
-	}
+        return compile(domElement.innerHTML);
+    }
 
-	function instantiate(jsTemplate, data) {
-		return compile(jsTemplate).render(data);
-	}
+    function instantiate(jsTemplate, data) {
+        return compile(jsTemplate).render(data);
+    }
 
-	function instantiateById(domId, data) {
-		if (/[^-0-9A-Z_a-z]/.test(domId)) {
-			throw new Error("invalid dom id");
-		}
+    // FIXME innerHTML sends '&lt;%' for '<%' and so as '%>'
+    function instantiateById(domId, data) {
+        if (/[^-0-9A-Z_a-z]/.test(domId)) {
+            throw new Error("invalid dom id");
+        }
 
-		// lookup cache first
-		if (typeof cache[domId] === "undefined") {
-			cache[domId] = compileByDomId(domId);
-		}
-		return cache[domId].render(data);
-	}
+        // lookup cache first
+        if (typeof cache[domId] === "undefined") {
+            cache[domId] = compileByDomId(domId);
+        }
+        return cache[domId].render(data);
+    }
 
-	this.jsTemplateEngine = {
-		instantiate: instantiate,
-		instantiateById: instantiateById
-	};
+    this.jsTemplateEngine = {
+        instantiate: instantiate,
+        instantiateById: instantiateById
+    };
 })();
