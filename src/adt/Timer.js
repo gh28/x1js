6f461fefe79544433dc9d37287d5898c9bb5c2cc
tanghao <tanghao.hb@gmail.com>
2018-09-03 21:19:36 +0800

    refine Timer

--- src/adt/Timer.js
+++ src/adt/Timer.js
@@ -25,43 +25,55 @@ Timer._setTimes = function(times) {
     this._times = times;
 };
 
-Timer.isRunning = function() {
-    return !isVoid(this._intervalToken);
-};
-
-Timer.schedule = function(callback, delay, times) {
-    this._setCallback(callback);
-    this._setDelay(delay);
-    this._setTimes(times);
-
-    this._onDelay = function() {
+Timer._setInterval = function() {
+    this._count = 0;
+    this._intervalToken = setInterval(function() {
         if (this._count++ < this._times) {
             this._callback();
         } else {
             this.stop();
         }
-    }.bind(this);
+    }.bind(this), this._delay);
+};
 
-    this._count = 0;
-    this._intervalToken = setInterval(this._onDelay, this._delay);
+Timer._clearInterval = function() {
+    clearInterval(this._intervalToken);
+    this._intervalToken = null;
+};
+
+Timer.isRunning = function() {
+    return !isVoid(this._intervalToken);
+};
+
+/**
+ * delay in milliseconds
+ */
+Timer.schedule = function(callback, delay, times) {
+    if (this.isRunning()) {
+        throw "E: timer is running";
+    }
+
+    this._setCallback(callback);
+    this._setDelay(delay);
+    this._setTimes(times);
+
+    this._setInterval();
 
     return this;
 };
 
 Timer.stop = function() {
     if (this.isRunning()) {
-        clearInterval(this._intervalToken);
-        this._intervalToken = null;
+        this._clearInterval();
     }
     return this;
 };
 
 Timer.reschedule = function() {
-    if (!isFunction(this._onDelay) || !isNumber(this._delay) || !isNumber(this._times)) {
+    if (!isFunction(this._callback) || !isNumber(this._delay) || !isNumber(this._times)) {
         throw "E: timer is not configured";
     }
     this.stop();
-    this._count = 0;
-    this._intervalToken = setInterval(this._onDelay, this._delay);
+    this._setInterval();
     return this;
 };
